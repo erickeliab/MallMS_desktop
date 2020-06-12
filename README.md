fbeaf7804626cf5485dddfe44fc094ce2ef64928
erick <erickemmanuel661@gmail.com>
2020-06-12 16:14:41 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Inventory Desktop Application
+
+Inventory Desktop application with Electron
+
+### Purpose
+Final Year Project for a friend
+
+## Usage
+
+### Installation
+
+Install the dependencies
+
+```sh
+$ npm install
+```
+
+### Serve
+To run electron
+
+```sh
+$ npm start
+```
+
+### To Package & Build (Still under early development, not adviced to build)
+
+For Windows
+
+```sh
+$ npm run package-win
+```
+
+For Mac
+
+```sh
+$ npm run package-mac
+```
+
+For Linux
+
+```sh
+$ npm run package-linux
+```
---
+++ views/navbar.js
@@ -0,0 +1,108 @@
+window.onload=function(){
+
+
+const Nav = () => {
+
+    //selecting element aside from the DOM
+    const navbarHolder =  document.getElementById('navnav');
+
+
+
+    //creating actual navbar (html)
+
+    //edit this nav by adding links and items
+    const navbar = `
+    <!-- Brand Logo -->
+    <a href="index3.html" class="brand-link">
+      
+      <span class="brand-text font-weight-light">InventoryApp</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+     
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item has-treeview menu-open">
+            <a href="index.html" class="nav-link">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Home
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="products.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Product</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="orders.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Orders</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="users.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Users</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="messages.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Messages</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="sales.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Sales</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="categories.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Categories</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="reports.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Report</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a href="login.html" class="nav-link">
+              <i class="nav-icon fas fa-th"></i>
+              <p>
+                Log Out
+                
+              </p>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+    `;
+
+
+    navbarHolder.insertAdjacentHTML( 'beforeend', navbar );
+
+    
+}
+
+Nav();
+
+  }
